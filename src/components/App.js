b7c8c424dc413372ec3b3786cd593ba5a0257505
Benarba Tewfik <tewfikhotspur@gmail.com>
2023-09-03 22:23:28 +0200

    Completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -4,13 +4,20 @@ import MultiButton from "./MultiButton";
 import ChangeItUp from "./ChangeItUp";
 import Login from "./Login";
 
+function Clickable({ clickEvent }) {
+  return <button onClick={clickEvent}>click me</button>;
+}
+
 function App() {
+  function eventHandle() {
+    console.log("you clicked me");
+  }
   return (
     <div>
       <h3>onClick</h3>
       <Tickler />
       <hr />
-
+      <Clickable clickEvent={eventHandle} />
       <MultiButton />
       <hr />
 
